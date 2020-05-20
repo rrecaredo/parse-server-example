ba083586aa2da78e559ef06b1bf892e52870ae36
Ricardo Recaredo <rrecaredo@twilio.com>
2020-05-20 17:07:41 +0200

    afdsfdafa

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ console.log('------', process.env);
 //   }
 // });
 
-// var app = express();
+var app = express();
 
-// app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // var mountPath = process.env.PARSE_MOUNT || '/parse';
 // app.use(mountPath, api);
 
 // // Parse Server plays nicely with the rest of your web routes
-// app.get('/', function(req, res) {
-//   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-// });
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+});
 
 app.get('/env', function(req, res) {
   res.json({ env: process.env });
