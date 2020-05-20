cd5ea85b80d169ec82d62f8652c6cb82d80677a8
Ricardo Recaredo <rrecaredo@twilio.com>
2020-05-20 17:22:50 +0200

    ggg

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ console.log('------', process.env);
 
 var app = express();
 
-app.use('/public', express.static(path.join(__dirname, '/public')));
+// app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // var mountPath = process.env.PARSE_MOUNT || '/parse';
 // app.use(mountPath, api);
