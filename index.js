aba7933c8ef851d85244e202e6f42183991483cf
Ricardo Recaredo <rrecaredo@twilio.com>
2020-05-20 17:01:28 +0200

    aaacc

--- index.js
+++ index.js
@@ -2,42 +2,42 @@
 // compatible API routes.
 
 var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+// var ParseServer = require('parse-server').ParseServer;
+// var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 console.log('------', process.env);
 
-var api = new ParseServer({
-  databaseURI: databaseUri,
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '',
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
-  liveQuery: {
-    classNames: ["Posts", "Comments", "GameScore"]
-  }
-});
+// var api = new ParseServer({
+//   databaseURI: databaseUri,
+//   appId: process.env.APP_ID || 'myAppId',
+//   masterKey: process.env.MASTER_KEY || '',
+//   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+//   liveQuery: {
+//     classNames: ["Posts", "Comments", "GameScore"]
+//   }
+// });
 
-var app = express();
+// var app = express();
 
-app.use('/public', express.static(path.join(__dirname, '/public')));
+// app.use('/public', express.static(path.join(__dirname, '/public')));
 
-var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
+// var mountPath = process.env.PARSE_MOUNT || '/parse';
+// app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
+// // Parse Server plays nicely with the rest of your web routes
+// app.get('/', function(req, res) {
+//   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+// });
 
 app.get('/env', function(req, res) {
   res.json({ env: process.env });
 });
 
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+// app.get('/test', function(req, res) {
+//   res.sendFile(path.join(__dirname, '/public/test.html'));
+// });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
@@ -46,5 +46,5 @@ httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
 
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+// // This will enable the Live Query real-time server
+// ParseServer.createLiveQueryServer(httpServer);
